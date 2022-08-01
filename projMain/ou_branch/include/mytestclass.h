61f85bc7a68a95f65a3e81ff3fe70c073b1c967d
DevJerryOu <oujieliang@outlook.com>
2022-08-01 11:53:42 +0800

    null

---
+++ projMain/ou_branch/include/mytestclass.h
@@ -0,0 +1,15 @@
+#if 0
+#ifndef __MYTESTCLASS_H__
+#define __MYTESTCLASS_H__
+
+#include <iostream>
+using namespace std;
+
+class MyTestClass{
+public:
+    static void print();
+};
+
+
+#endif
+#endif
---
+++ projMain/ou_branch/src/mytestclass.cc
@@ -0,0 +1,8 @@
+#if 0
+#include "mytestclass.h"
+
+void MyTestClass::print(){
+        cout << "MyTestClass::print()->hello!" << endl;
+}
+
+#endif
